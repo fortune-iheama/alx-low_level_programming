5cfc5f25f4a79e7b0c7576edf5b5f9b87283f0ff
mfoniso <fortuneibok@yahoo.com>
2023-04-24 06:46:56 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,17 @@
 */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-putchar(i + '0');
-if (i ! = 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n')
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
