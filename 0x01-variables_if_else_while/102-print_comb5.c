343e32af29a5df9645933f2aefd10ce0567646fa
mfoniso <fortuneibok@yahoo.com>
2023-04-24 17:37:48 -0700

    102 print comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:Print all possible
+ * different combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(32);
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 == 98 && n2 == 99)
+				continue;
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
