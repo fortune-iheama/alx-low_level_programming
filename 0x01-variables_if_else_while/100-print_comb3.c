c4da361b126a06586d2052abaeb668d7a7238858
mfoniso <fortuneibok@yahoo.com>
2023-04-24 17:41:45 -0700

    100 print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- * Description: a program that prints all possible combinations of two two-digit numbers
+ * Description:Print all possible
+ * different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
