424548582d7a4081198c9131e81bc18ac102624b
mfoniso <fortuneibok@yahoo.com>
2023-04-26 15:33:14 -0700

    1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
-  * main - Write a function that prints the alphabet
-  * in lowercase, followed by a new line.
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
   * Return: void
   */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c;
+	int i = 0;
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (i <= 9)
 	{
-		_putchar(c);
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+
+		i++;
+	}
 }
+
