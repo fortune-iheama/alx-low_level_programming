80c6e74c7d357a5516cd14ce069178e02a46fa30
mfoniso <fortuneibok@yahoo.com>
2023-04-25 20:31:28 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabets - must print in lowercase
+ */
+
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar('\n');
+}
