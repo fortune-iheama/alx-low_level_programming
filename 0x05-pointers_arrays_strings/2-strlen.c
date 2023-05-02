88d0c6c612f769a840df67a897befbc5f7ceac70
mfoniso <fortuneibok@yahoo.com>
2023-05-02 16:21:11 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,7 @@
-#include "main.h"
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted
+ * Return: The integer value of the converted string
  */
 int _atoi(char *s)
 {
