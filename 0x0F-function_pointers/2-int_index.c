505da9d4cfe39c03ccd707a072f09684ddff2abd
mfoniso <fortuneibok@yahoo.com>
2023-05-25 16:31:32 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - main function
+ * @array: array
+ * @size: number
+ * @cmp: pointer
+ * Return: if size <=0 or no element matches,
+ * return -1, otherwise return the index of
+ * the first element for which the cmp function
+ * does not return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
